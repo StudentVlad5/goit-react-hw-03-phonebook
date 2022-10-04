f19f5a3801a6a44b5424a7b52669ef41402c8883
StudentVlad5 <99014125+StudentVlad5@users.noreply.github.com>
2022-10-04 19:05:25 +0300

    finished this project

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,24 +2,28 @@ import React,{ Component } from 'react';
 import Filter from './Filter/Filter'
 import ContactForm from './ContactForms/ContactForm';
 import ContactList from './ContactList/ContactList';
+import LocalStorage from './LocalStorage/LocalStorage';
+import DynamicSort from './DynamicSort/DynamicSort'
 import initialContacts from './initialContacts.json'
 
-function dynamicSort(property) {
-  return function (a,b) {
-        const result = (a[property] < b[property]) ? -1 : (a[property] > b[property]) ? 1 : 0;
-        return result;
-  }
-}
-initialContacts.sort(dynamicSort("name"));
+
+let CheckState = Object.assign(initialContacts, JSON.parse(localStorage.getItem('initialContacts'))).sort(DynamicSort("name"))
 
 export default class App extends Component {
 state = {
-      contacts: initialContacts,
+      contacts: CheckState,
       filter:'',
      }
 
-handleAddContact = (newContact) => {this.setState(({contacts})=>({contacts : [...contacts, newContact],})
-)}
+componentDidUpdate() {
+LocalStorage(this.state.contacts);
+console.log(JSON.parse(localStorage.getItem('initialContacts')))
+}   
+  
+handleAddContact = (newContact) => {
+  this.setState(({contacts})=>({contacts : [...contacts, newContact],
+}))
+}
 
 handleCheckUniqueContact = (name) => {
   const {contacts} = this.state;
@@ -51,7 +55,7 @@ return contacts.filter((contact) => contact.name.toLowerCase().includes(filter.t
         color: '#010101'
       }}>
 <ContactForm onAdd={this.handleAddContact} onCheckUnique={this.handleCheckUniqueContact}/>
-<h2>Contacts List</h2>
+<h2>Contacts List: {visibleContacts.length}</h2>
 <h5>Find contact</h5><Filter filter={filter} onChange={this.handleFilterChange}/>
 <div className="list_section">
 <ContactList contacts={visibleContacts} onRemove={this.handleRemoveContact}/>
